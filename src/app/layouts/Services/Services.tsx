a9c04da8bbc90e21d34aca1fa2a9656a26b29e11
douglas-front <douglasjknw007@gmail.com>
2025-06-10 12:03:01 -0300

    feat: scroll smoother gsap added

---
+++ src/app/layouts/Services/Services.tsx
@@ -0,0 +1,19 @@
+import styles from './Services.module.css'
+
+export default function Services() {
+    return (
+        <section className={styles.services}>
+            <div className={styles.texts}>
+                <p>
+                    A Aquatec é especialista em desentupimento de tudo, <br />
+                    oferecendo serviços completos para residências, <br />
+                    comércios, indústrias e condomínios. Trabalhamos com <br />
+                    tecnologia moderna, equipe qualificada e atendimento <br />
+                    ágil para resolver qualquer tipo de entupimento, <br />
+                    garantindo segurança, limpeza e eficiência.
+                </p>
+                <h2>SERVIÇOS</h2>
+            </div>
+        </section>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,51 @@
+'use client'
+
+import { useEffect } from "react";
 import Nav from "./components/Nav/Nav";
 import About from "./layouts/About/About";
 import Hero from "./layouts/Hero/Hero";
 import Preloader from "./layouts/Preloader/Preloader"
+import Services from "./layouts/Services/Services";
+import gsap from "gsap";
+import ScrollSmoother from "gsap/ScrollSmoother";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 export default function Home() {
+
+  useEffect(() => {
+
+    gsap.registerPlugin(ScrollSmoother, ScrollTrigger)
+
+    ScrollSmoother.create({
+      wrapper: '#smooth-wrapper',
+      content: '#smooth-content',
+
+      smooth: 2,
+      smoothTouch: 2
+    });
+
+  }, [])
+
   return (
     <>
-      {/* <Preloader/>   */}
+      <Preloader/>  
       <header>
-        <Nav/>
+        <Nav />
       </header>
       <main>
-        <div className="line"></div>
-        <div className="line"></div>
-        <Hero />
-        <About/>
-        {/* <About/> */}
+
+        <div id="smooth-wrapper">
+          <div id="smooth-content">
+
+            <div className="line"></div>
+            <div className="line"></div>
+
+            <Hero />
+            <About />
+            <Services />
+            {/* <About/> */}
+          </div>
+        </div>
       </main>
     </>
   );
